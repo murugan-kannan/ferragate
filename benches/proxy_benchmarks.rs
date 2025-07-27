d610c4bfc3b935842eaaccb52820b48a432daf69
Murugan Kannan <machaendran9221@gmail.com>
2025-07-27 11:55:10 +0530

    upgrade benchmark dependencies
    - Updated Cargo.toml with benchmark dependencies and configurations
    - Refreshed Cargo.lock with latest dependency versions

--- benches/proxy_benchmarks.rs
+++ benches/proxy_benchmarks.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use std::hint::black_box;
 use ferragate::config::{GatewayConfig, RouteConfig, ServerConfig};
 use ferragate::proxy::ProxyState;
 use std::collections::HashMap;
